09876b43f7078ef74a19fee8e0765e3050c82225
shanvit <shanvit7@gmail.com>
2023-06-17 12:58:09 +0530

    overflow removed from home page

--- app/home/layout.jsx
+++ app/home/layout.jsx
@@ -3,7 +3,7 @@ import TopNavbar from "../components/Navigation/TopNavbar";
 
 const HomeLayout = ({ children }) => {
   return (
-    <section className="w-screen h-screen">
+    <section className="w-screen h-screen overflow-x-hidden">
       <TopNavbar pageTitle="Home" />
       {children}
     </section>
